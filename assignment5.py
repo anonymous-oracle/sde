0af62c48ceb9390973aa5ac66ab8a9c60e9e2d4a
suhasks96 <suhas.ks@thesmartq.com>
2026-01-19 15:46:14 +0530

    added changes

--- assignment5.py
+++ assignment5.py
@@ -1,45 +1,32 @@
 character_name = "Ashborn - The Shadow Monarch (The greatest fragment of the brilliant light)"
 
-class Hunter:
-    # The Setup (Constructor)
-    def __init__(self, name, rank):
-        self.name = name       # Attribute
-        self.rank = rank       # Attribute
-        self.hp = 100          # Default Attribute
-        self.inventory = []    # Each Hunter gets their own backpack!
-        self.damage = 30
-
-    # An Action (Method)
-    def show_stats(self):
-        print(f"Hunter: {self.name} | Rank: {self.rank} | HP: {self.hp}")
-
-    def take_damage(self, damage):
-        self.hp -= damage
-        print(f"{self.name} took {damage} damage! HP is now {self.hp}")
-
-    def pick_up_item(self, item):
-        self.inventory.append(item)
-        print(f"{self.name} obtained {item}!")
-    
-    def attack(self, target):
-        # target is going to be our Player object!
-        print(f"{self.name} attacks {target.name}!")
-        target.take_damage(self.damage)
-
-class Monster:
+class Character:
     def __init__(self, name, hp, damage):
         self.name = name
         self.hp = hp
         self.damage = damage
-
+    
     def attack(self, target):
-        # target is going to be our Player object!
         print(f"{self.name} attacks {target.name}!")
         target.take_damage(self.damage)
     
     def take_damage(self, damage):
         self.hp -= damage
-        print(f"{self.name} took {damage} damage! HP is now {self.hp}")
+        print(f"{self.name} took {damage} damage! HP: {self.hp}")
+        
+class Hunter(Character):
+    def __init__(self, name, rank):
+        super().__init__(name, 100, 30)
+        self.rank = rank
+        self.inventory = []
+
+    def pick_up_item(self, item):
+        self.inventory.append(item)
+        print(f"{self.name} obtained {item}!")
+
+
+class Monster(Character):
+    pass
 
 player = Hunter("Ashborn", "The Shadow Monarch")
 boss = Monster("Igris - The Red", 200, 15)
