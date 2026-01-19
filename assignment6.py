f8eae7254196116c7c9598630e0e2c6d0768a9f4
suhasks96 <suhas.ks@thesmartq.com>
2026-01-19 16:05:57 +0530

    assignment 7 started

---
+++ assignment6.py
@@ -0,0 +1,70 @@
+import sqlite3
+
+class DatabaseManager:
+    def __init__(self):
+        # Connect to the file 'game_data.db'
+        self.conn = sqlite3.connect('game_data.db')
+        self.cursor = self.conn.cursor()
+        self.create_table()
+
+    def create_table(self):
+        # Create the table if it doesn't exist yet
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS players (
+                name TEXT PRIMARY KEY,
+                rank TEXT,
+                hp INTEGER
+            )
+        ''')
+        self.conn.commit() # Save changes
+
+    def save_player(self, player):
+        # "Upsert": Insert if new, Replace if exists
+        self.cursor.execute('''
+            INSERT OR REPLACE INTO players (name, rank, hp)
+            VALUES (?, ?, ?)
+        ''', (player.name, player.rank, player.hp))
+        
+        self.conn.commit()
+        print(f"--- GAME SAVED: {player.name} ---")
+
+class Character:
+    def __init__(self, name, hp, damage):
+        self.name = name
+        self.hp = hp
+        self.damage = damage
+    
+    def attack(self, target):
+        print(f"{self.name} attacks {target.name}!")
+        target.take_damage(self.damage)
+    
+    def take_damage(self, damage):
+        self.hp -= damage
+        print(f"{self.name} took {damage} damage! HP: {self.hp}")
+        
+class Hunter(Character):
+    def __init__(self, name, rank):
+        super().__init__(name, 100, 30)
+        self.rank = rank
+        self.inventory = []
+
+    def pick_up_item(self, item):
+        self.inventory.append(item)
+        print(f"{self.name} obtained {item}!")
+
+
+class Monster(Character):
+    pass
+
+player = Hunter("Ashborn", "The Shadow Monarch")
+boss = Monster("Igris - The Red", 200, 15)
+
+while True:
+    player.attack(boss)
+    print("You hit the boss!")
+    boss.attack(player)
+    if player.hp > 0 and boss.hp > 0:
+        break
+
+db = DatabaseManager()
+db.save_player(player)
