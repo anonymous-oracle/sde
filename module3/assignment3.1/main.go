0883fd80926db884f2144c30e1c23be6f4f7eada
suhasks96 <suhas.ks@thesmartq.com>
2026-05-23 01:00:21 +0530

    module 3 wip

---
+++ module3/assignment3.1/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+type AgentCard struct {
+	ID int
+	Name string
+	PowerLevel float64
+}
+
+type ControlPlane struct { 
+	ActiveAgent AgentCard
+}
+
+func main() {
+	var myAgent AgentCard = AgentCard{ID: 101, Name: "Nasiko-01", PowerLevel: 9.5}
+	var controlPlane ControlPlane = ControlPlane{ActiveAgent: myAgent}
+
+	fmt.Printf("%+v\n", controlPlane)
+}
