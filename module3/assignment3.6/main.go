23ac7aa4d335419537214bae9ba15a67f247a910
suhasks96 <suhas.ks@thesmartq.com>
2026-05-24 00:49:21 +0530

    assignment 3.6 done

---
+++ module3/assignment3.6/main.go
@@ -0,0 +1,67 @@
+package main
+
+import "fmt"
+
+
+type Telemetry struct {
+	LatencyMs int
+	PayloadBytes int
+}
+
+func (telemetry_ Telemetry) CalculateThroughput() (float64) {
+	if telemetry_.LatencyMs == 0 {return 0.0} // 0 latency is theoretically impossible
+	return float64(telemetry_.PayloadBytes) / float64(telemetry_.LatencyMs)
+}
+
+type SystemNode struct {
+	UUID int
+	LoadAverage float64
+}
+
+func (node *SystemNode) IngestLoad(currentLoad float64) {
+	node.LoadAverage = currentLoad
+}
+
+type ProcessCluster struct {
+	IsHalted bool
+}
+
+func (pc *ProcessCluster) Halt() {
+	if pc==nil {
+		fmt.Println("Aborting: Cluster reference unallocated")
+		return
+	}
+	pc.IsHalted = true
+	fmt.Println("Cluster safely locked down.")	
+}
+
+type DataKernel struct {
+	Size int
+}
+
+func (kernel *DataKernel) Expand() {
+	kernel.Size += 1024
+}
+
+type StorageNode struct {
+	Kernel DataKernel
+}
+
+type SecurityPolicy struct {
+	Enforced bool
+	Level int
+	Signature string
+}
+
+func (policy SecurityPolicy) VerifyCompliance() (bool) {
+	return policy.Enforced && policy.Level > 5
+}
+
+func main(){
+	var storageNode StorageNode = StorageNode{DataKernel{Size: 512}}
+	storageNode.Kernel.Expand()
+	fmt.Printf("The updated size of the kernel is %d\n", storageNode.Kernel.Size)
+
+	var secPolicy SecurityPolicy = SecurityPolicy{Enforced: true, Level: 6, Signature: "Test"}
+	fmt.Println(secPolicy.VerifyCompliance())
+}
