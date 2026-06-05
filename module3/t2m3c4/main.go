e6f22e98db82703994954016bf55346a0ed3b3cc
suhasks96 <suhas.ks@thesmartq.com>
2026-06-05 16:39:00 +0530

    t2m3c4 wip

---
+++ module3/t2m3c4/main.go
@@ -0,0 +1,24 @@
+package main
+
+type ClusterState int 
+const (
+	StateHalted = iota
+	StateOperational
+	StateOverloaded
+)
+
+type MetricEnvelope[T any] struct {
+	SystemState ClusterState
+	Payload T
+}
+
+type SubsystemRegistry[K comparable, V any] struct {
+	MetricEnvelopes map[K]MetricEnvelope[V]
+}
+
+func (sr *SubsystemRegistry[K, V]) RegisterAndAudit(key K, data V, state ClusterState) (V, bool) {
+	if val, ok := sr.MetricEnvelopes[key]; ok {
+		return val.Payload, false
+	}
+	
+}
