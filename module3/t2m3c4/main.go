577ce6d81c98e5fb284c877085f7f33ffcdab20b
suhasks96 <suhas.ks@thesmartq.com>
2026-06-10 21:33:25 +0530

    t2m3c4 wip

--- module3/t2m3c4/main.go
+++ module3/t2m3c4/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type ClusterState int
 
 const (
@@ -18,20 +20,30 @@ type SubsystemRegistry[K comparable, V any] struct {
 }
 
 func (sr *SubsystemRegistry[K, V]) RegisterAndAudit(key K, data V, state ClusterState) (V, bool) {
+	var returnPayload V
+	var boolVal bool
 	if val, ok := sr.MetricEnvelopes[key]; ok {
-		return val.Payload, false
+		returnPayload = val.Payload
+		boolVal = false
 	} else if !ok {
 		sr.MetricEnvelopes[key] = MetricEnvelope[V]{SystemState: state, Payload: data}
-		return data, true
-	} else if state == StateOverloaded {
-		var zeroVal V
-		return zeroVal, false
+		returnPayload = data
+		boolVal = true
 	}
-	return data, false
+	if state == StateOverloaded {
+		var zeroVal V
+		returnPayload = zeroVal
+		boolVal = false
+	}
+	return returnPayload, boolVal
 }
 
 func main() {
 	var registry SubsystemRegistry[string, int] = SubsystemRegistry[string, int]{MetricEnvelopes: (make(map[string]MetricEnvelope[int]))}
-	registry.RegisterAndAudit("core-01", 4096, StateOperational)
-	registry.RegisterAndAudit("core-01", 8192, StateOperational)
+	res, _ := registry.RegisterAndAudit("core-01", 4096, StateOperational)
+	fmt.Println(res)
+	res, _ = registry.RegisterAndAudit("core-01", 8192, StateOperational)
+	fmt.Println(res)
+	res, _ = registry.RegisterAndAudit("core-overflow", 9999, StateOverloaded)
+	fmt.Println(res)
 }
